c911bc32abce184b5dd3dcfebe9fcb86290a05e8
welopez <57297784+welopez@users.noreply.github.com>
2021-05-29 15:04:04 -0300

    Creación del proyecto

---
+++ Actividad3/app/src/main/java/com/ppc_tp4_isea_lopez/actividad3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ppc_tp4_isea_lopez.actividad3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Actividad3/app/src/test/java/com/ppc_tp4_isea_lopez/actividad3/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.ppc_tp4_isea_lopez.actividad3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
